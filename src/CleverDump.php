88f27120141d4a7fa0cf16655ce58f4e3557dafa
Mikulas <rullaf@gmail.com>
2013-08-31 10:19:22 +0200

    thow user error if there are multiple call per line

--- src/CleverDump.php
+++ src/CleverDump.php
@@ -2,7 +2,7 @@
 
 function d($d)
 {
-	static $lastCall;
+	static $linesWarned;
 
 	$trace = debug_backtrace(NULL, 1)[0];
 	$hitline = $trace['line'];
@@ -32,10 +32,39 @@ function d($d)
 		}
 	};
 
-	// skip whitespace
-	while ($token[0] === T_WHITESPACE) {
-		$token = $tokens[$ptr];
-		$ptr++;
+	// detect if there are any other calls to this function on the same line
+	$xptr = $ptr;
+	$duplicate = FALSE;
+	while (TRUE) {
+		$token = $tokens[$xptr];
+		$xptr++;
+		if (!is_array($token)) {
+			continue;
+		}
+		if ($token[0] === T_STRING && strToLower($token[1]) === strToLower(__FUNCTION__)) {
+			$duplicate = TRUE;
+			break;
+		}
+		if ($token[2] !== $line) {
+			break;
+		}
+	};
+
+	if ($duplicate) {
+		$key = $trace['file'] . "|" . $trace['line'];
+		if (!isset($linesWarned[$key])) {
+			$linesWarned[$key] = TRUE;
+			$f = __FUNCTION__;
+			trigger_error("Invalid usage of $f(): only one call per line is supported.", E_USER_ERROR);
+		}
+	}
+
+	// skip whitespace between function name and parenthesis
+	if ($tokens[$ptr][0] === T_WHITESPACE) {
+		do {
+			$token = $tokens[$ptr];
+			$ptr++;
+		} while ($token[0] === T_WHITESPACE);
 	}
 
 	// if function call, parenthesis must follow
--- tests/multiple.phpt
+++ tests/multiple.phpt
@@ -2,26 +2,12 @@
 
 require __DIR__ . '/bootstrap.php';
 
-// this is a tough one
-Assert::output(function() {
+// This is actually solvable, but I don't
+// really see the use-case.
+Assert::error(function() {
 	d( d('test') );
-}, "'test' = 'test'\nd('test') = 'test");
+}, E_USER_ERROR);
 
-Assert::output(function() {
+Assert::error(function() {
 	d('a'); d('b');
-}, "'a' = 'a'\n'b' = 'b'");
-
-Assert::output(function() {
-	for ($i = 0; $i < 2; ++$i) {
-		d('a'); d('b');
-	}
-}, "this is a hard one");
-
-// Having a 50:50 chance, it seems impossible to
-// determine which of the functions was called;
-// both calls have identical stack trace.
-$chance = mt_rand(0, 1);
-$l = $chance ? 'a' : 'b';
-Assert::output(function() use ($chance) {
-	$chance ? d('a') : d('b');
-}, "'$l' = '$l'");
+}, E_USER_ERROR);
